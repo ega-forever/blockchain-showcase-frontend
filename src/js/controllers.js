049c4ea1f2b90543f11738ae6b0e3474bdabec26
zyevegor <zyev.egor@gmail.com>
2016-10-05 14:47:19 +0300

    initial commit

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -4,41 +4,44 @@ angular.module('controllers', ['services'])
     $scope.messages = [];
     $scope.newMessage = {};
 
-    restFactory.getCollection().then(function(data){
-      $scope.messages = data.data
+    restFactory.getCollection().then(function (data) {
+      $scope.messages = data.data;
+      setInterval(function () {
+        restFactory.getCollection().then(function (data) {
+          $scope.messages = data.data
+        });
+      }, 3000);
     });
 
 
-
-    $scope.get = function(message){
-      if(message.body){
+    $scope.get = function (message) {
+      if (message.body) {
         return;
       }
-      restFactory.getBody(message._id).then(function(data){
-          _.merge(message, {body: data.data.body});
+      restFactory.getBody(message._id).then(function (data) {
+        _.merge(message, {body: data.data.body});
       })
     };
 
-    $scope.push = function(){
+    $scope.push = function () {
       $scope.messages.push(Object.create($scope.newMessage));
       $scope.newMessage = {};
     };
 
-    $scope.pull = function(message){
-      restFactory.delete(message._id).then(function(data){
+    $scope.pull = function (message) {
+      restFactory.delete(message._id).then(function (data) {
         _.pull($scope.messages, message)
       });
     };
 
 
-    $scope.update = function(message){
+    $scope.update = function (message) {
       restFactory.edit(_.pick(message, ['_id', 'body', 'header']))
-        .then(function(data){
-        message.edit.header = false;
-        message.edit.body = false;
-      })
+        .then(function (data) {
+          message.edit.header = false;
+          message.edit.body = false;
+        })
     }
-
 
 
   });
